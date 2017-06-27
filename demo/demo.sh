9c5b0e88fef6532f8bf3580896fec9947848123f
Axel Berardino <axel.berardino@gmail.com>
2017-06-28 01:59:58 +0200

    Handle warn and update todo list.

--- demo/demo.sh
+++ demo/demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 BASE_DIR=/tmp/mykafka-demo
-#DEMOS="simple-producer-consumer.sh ctl-orders.sh one-producer-many-consumers.sh"
-DEMOS="one-producer-many-consumers.sh"
+DEMOS="simple-producer-consumer.sh ctl-orders.sh one-producer-many-consumers.sh"
+#DEMOS="one-producer-many-consumers.sh"
 
 trap "echo" SIGTERM
 
@@ -31,4 +31,10 @@ done
 #
 # Test delete partition, while read.
 # Test delete topic, while read.
-# Test gros bench, 1 writer, 16 lecteurs infinis.
+
+# => demander press -p + htop en "warn"
+# Test gros bench 1, 1 partition, 8 writer, 16 lecteurs infinis.
+# Test gros bench 2, 8 partitions, * (1 writer, 2 lecteurs infinis).
+
+# 1 writer: max partition size 40 Mo, segsize 4 Mo
+# 1 writer: ttl 1 sec, segsize 1024 * 1024 (1 Mo). Constater qu'il ne reste qu'un seul log
